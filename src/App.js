4c2dc536fa814556e55cc2664db0af6d9ebc445b
Richie559 <richardnavarro559@gmail.com>
2022-05-09 10:02:28 -0700

    richard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import Main from './Main.js';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { countries } from './data/countries.js';
+import Test from './Test.js';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//const [path, setPath] = useState('wordlwide');
+	const [country, setCountry] = useState('worldwide');
+	//const [countryName, setCountryName] = useState('');
+	const navigate = useNavigate();
+	const countryChangeHandler = (countryName) => {
+		//setPath('/worldwide/' + countryName);
+		setCountry(countryName);
+		let myCountry = countries.find((c) => c.Code === countryName);
+		setCountry(myCountry.Name);
+		navigate('/worldwide/' + myCountry.Name);
+	};
+	return (
+		<Routes>
+			<Route path='/' element={<Main onCountry={countryChangeHandler} />} />
+			<Route
+				path='/worldwide'
+				element={<Main onCountry={countryChangeHandler} />}
+			>
+				<Route path={country} element={<Test />}></Route>
+			</Route>
+		</Routes>
+	);
 }
-
 export default App;
